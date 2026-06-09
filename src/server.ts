03f260f7d712bb3257e4d75af25c5d32cf03ee1e
Adnan jamal <jamaladnan982@gmail.com>
2026-06-09 17:18:32 +0530

    Fix CORS for Vercel

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,8 @@ const app = express(); // Yeh express app banati hai
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:[ "http://localhost:5173",
+     "https://reactjs-ecom-fullstack-app.vercel.app"],
     credentials: true,
   }),
 );
